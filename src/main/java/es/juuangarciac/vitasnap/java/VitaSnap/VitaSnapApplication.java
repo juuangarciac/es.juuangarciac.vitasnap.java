ec212344ac0e6aafcc35fd2eb86024f1dc0c63e7
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2024-01-17 17:27:51 +0100

    Initial commit

---
+++ src/main/java/es/juuangarciac/vitasnap/java/VitaSnap/VitaSnapApplication.java
@@ -0,0 +1,13 @@
+package es.juuangarciac.vitasnap.java.VitaSnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaSnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/juuangarciac/vitasnap/java/VitaSnap/VitaSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package es.juuangarciac.vitasnap.java.VitaSnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaSnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
