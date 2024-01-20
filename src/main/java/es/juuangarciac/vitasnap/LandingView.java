7b4383184ed9f37155e4cff9a4b36d067c8a6c3b
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2024-01-20 19:00:10 +0100

    Update homeView

--- src/main/java/es/juuangarciac/vitasnap/LandingView.java
+++ src/main/java/es/juuangarciac/vitasnap/LandingView.java
@@ -3,26 +3,20 @@ package es.juuangarciac.vitasnap;
 import com.flowingcode.vaadin.addons.carousel.Carousel;
 import com.flowingcode.vaadin.addons.carousel.Slide;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
+
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Paragraph;
+
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.server.VaadinService;
+
+import com.vaadin.flow.server.StreamResource;
+
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin.Vertical;
 
-import es.juuangarciac.vitasnap.user.views.UserActivationView;
-import es.juuangarciac.vitasnap.user.views.UserHomeView;
-import es.juuangarciac.vitasnap.user.views.UserRegistrationView;
 import jakarta.annotation.PostConstruct;
-
-import org.apache.catalina.webresources.FileResource;
 import org.springframework.beans.factory.annotation.Value;
 
 @PageTitle("Home")
@@ -49,20 +43,35 @@ public class LandingView extends VerticalLayout {
             verticalLayout.setJustifyContentMode(JustifyContentMode.CENTER);
 
         HorizontalLayout horizontalLayout = new HorizontalLayout(verticalLayout, createCarousel());
-        
         add(horizontalLayout);
     }
 
     public Carousel createCarousel() {
+        // Slide : 1
         Slide slide1 = new Slide();
-        Image img = new Image("frontend\\themes\\img_1.jpg", "Tres personas haciendo surf al atardecer");
-        slide1.add(img);
+        StreamResource imageResource = new StreamResource("image",
+        () -> getClass().getResourceAsStream("images/img_1.jpg"));
+        Image image = new Image(imageResource, "Tres personas haciendo surf al atardecer");
+            image.setHeight("553.5px");
+            image.setWidth("368px");
+        slide1.add(image);
+
+        // Slide : 2
+        /*TO-DO:
+            Slide slide2 = new Slide();
+            imageResource = new StreamResource("image",
+            () -> getClass().getResourceAsStream("images/img_2.jpg"));
+            image = new Image(imageResource, "Gente Feliz de fiesta");
+                image.setHeight("553.5px");
+                image.setWidth("368px");
+            slide2.add(image);
+        */
 
         Carousel carousel = new Carousel(slide1)
-        .withAutoProgress()
-        .withoutSwipe()
-        .withSlideDuration(4)
-        .withStartPosition(0);
+            .withAutoProgress()
+            .withoutSwipe()
+            .withSlideDuration(4)
+            .withStartPosition(0);
 
         return carousel;
     }
