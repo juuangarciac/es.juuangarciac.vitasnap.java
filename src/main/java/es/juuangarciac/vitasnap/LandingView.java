c55d9609bd6b178ba3cc2dbb34eb9e5a5f6f0703
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2024-01-20 17:59:11 +0100

    Update home

--- src/main/java/es/juuangarciac/vitasnap/LandingView.java
+++ src/main/java/es/juuangarciac/vitasnap/LandingView.java
@@ -1,17 +1,28 @@
 package es.juuangarciac.vitasnap;
 
+import com.flowingcode.vaadin.addons.carousel.Carousel;
+import com.flowingcode.vaadin.addons.carousel.Slide;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.VaadinService;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin.Vertical;
+
 import es.juuangarciac.vitasnap.user.views.UserActivationView;
 import es.juuangarciac.vitasnap.user.views.UserHomeView;
 import es.juuangarciac.vitasnap.user.views.UserRegistrationView;
 import jakarta.annotation.PostConstruct;
+
+import org.apache.catalina.webresources.FileResource;
 import org.springframework.beans.factory.annotation.Value;
 
 @PageTitle("Home")
@@ -32,22 +43,28 @@ public class LandingView extends VerticalLayout {
 
     @PostConstruct
     public void init() {
-
         // Este método se ejecuta después de que se ejecute el constructor y se inyecten las dependencias (appVersion)
+        
+        VerticalLayout verticalLayout = new VerticalLayout(new H1("VitaSnap"), new H3("Momentos inolvidables. Para gente inolvidable"));
+            verticalLayout.setJustifyContentMode(JustifyContentMode.CENTER);
 
-        add(new H1("VitaSnap"));
-        add(new H2("Curso 2023/2024"));
-        add(new H2("Bienvenid@ a la aplicación demo"));
-        add(new H3("Versión: " + appVersion));
+        HorizontalLayout horizontalLayout = new HorizontalLayout(verticalLayout, createCarousel());
+        
+        add(horizontalLayout);
+    }
 
-        RouterLink homeLink = new RouterLink("User Home view", UserHomeView.class);
-        add(homeLink);
+    public Carousel createCarousel() {
+        Slide slide1 = new Slide();
+        Image img = new Image("frontend/img/home/img_1.jpg", "Tres personas haciendo surf al atardecer");
+        slide1.add(img);
 
-        RouterLink userRegistrationLink = new RouterLink("User Registration view", UserRegistrationView.class);
-        add(userRegistrationLink);
+        Carousel carousel = new Carousel(slide1)
+        .withAutoProgress()
+        .withoutSwipe()
+        .withSlideDuration(4)
+        .withStartPosition(0);
 
-        RouterLink userActivationLink = new RouterLink("User Activation view", UserActivationView.class);
-        add(userActivationLink);
+        return carousel;
     }
 
 }
