509f7af1457e2c19e2164f9226f30e47d1450451
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2024-02-11 19:19:54 +0100

    Fix Errors Funciontal

--- src/main/java/es/juuangarciac/vitasnap/LoadDatabase.java
+++ src/main/java/es/juuangarciac/vitasnap/LoadDatabase.java
@@ -4,34 +4,44 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import es.juuangarciac.vitasnap.user.domain.Role;
 import es.juuangarciac.vitasnap.user.domain.User;
 import es.juuangarciac.vitasnap.user.repositories.UserRepository;
+import es.juuangarciac.vitasnap.user.security.SecurityConfiguration;
 
 @Configuration
 class LoadDatabase {
+  private final PasswordEncoder passwordEncoder = SecurityConfiguration.passwordEncoder();
 
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
+  @Autowired
   @Bean
   CommandLineRunner initDatabase(UserRepository repository) {
+
+    if(!repository.findAll().isEmpty()) return args -> {};
+
         User admin = new User();
           admin.setUsername("admin");
-          admin.setPassword("admin");
+          admin.setPassword(passwordEncoder.encode("admin"));
           admin.setEmail("admin@gmail.com");
           admin.setRegisterCode(UUID.randomUUID().toString().substring(0, 5));
           admin.addRole(Role.ADMIN);
+          admin.setActive(true);
         
         User user = new User();
             user.setUsername("user");
-            user.setPassword("user");
+            user.setPassword(passwordEncoder.encode("user"));
             user.setEmail(user.getUsername() + "@gmail.com");
             user.setRegisterCode(UUID.randomUUID().toString().substring(0, 5));
             user.addRole(Role.USER);
+            user.setActive(true);
 
     return args -> {
       log.info("Preloading " + repository.save(admin));
--- src/main/java/es/juuangarciac/vitasnap/user/security/SecurityConfiguration.java
+++ src/main/java/es/juuangarciac/vitasnap/user/security/SecurityConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfiguration {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
