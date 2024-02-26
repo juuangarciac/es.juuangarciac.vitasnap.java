2c12ab9e330aa716d23aa66f3ebebd2eb1741658
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2024-02-26 15:57:06 +0100

    Add API keys security

--- src/main/java/es/juuangarciac/vitasnap/WebApplication.java
+++ src/main/java/es/juuangarciac/vitasnap/WebApplication.java
@@ -2,8 +2,10 @@ package es.juuangarciac.vitasnap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class WebApplication{
 
     public static void main(String[] args) {
--- src/main/java/es/juuangarciac/vitasnap/security/SecurityConfiguration.java
+++ src/main/java/es/juuangarciac/vitasnap/security/SecurityConfiguration.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,15 +22,13 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity
-        .formLogin(Customizer.withDefaults())
-        .authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/api/*").authenticated();
-            auth.requestMatchers("/api/*").hasRole("ADMIN");
-            auth.anyRequest().authenticated();
-        })
-        .build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+       http.csrf(AbstractHttpConfigurer::disable)
+          .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.requestMatchers("/**").authenticated())
+          .httpBasic(Customizer.withDefaults())
+          .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+          .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
 }
